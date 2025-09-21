33a220f3058772b9089458e79a21dad655aa0532
Joker-V2 <nipinna0@gmail.com>
2025-09-21 10:08:21 +0200

    fs: fixup

--- fs/ext4/dir.c
+++ fs/ext4/dir.c
@@ -62,7 +62,7 @@ int __ext4_check_dir_entry(const char *function, unsigned int line,
 			   struct inode *dir, struct file *filp,
 			   struct ext4_dir_entry_2 *de,
 			   struct buffer_head *bh, char *buf, int size,
-			   unsigned int offset)
+			   ext4_lblk_t block, unsigned int offset)
 {
 	const char *error_msg = NULL;
 	const int rlen = ext4_rec_len_from_disk(de->rec_len,
