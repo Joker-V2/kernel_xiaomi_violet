b437cdfdbde7772a99e1ef1ecaf4648f532b6115
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-14 10:55:20 +0100

    Update ksuinit.c

--- drivers/kernelsu/ksuinit.c
+++ drivers/kernelsu/ksuinit.c
@@ -162,3 +162,10 @@ MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
 #endif
+#if !defined(MODULE)
+/*
+ * Built-in KernelSU (CONFIG_KSU=y)
+ * Ensure init is called for built-in driver
+ */
+late_initcall(kernelsu_init);
+#endif
