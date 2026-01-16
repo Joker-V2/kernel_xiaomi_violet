fdb9816a8d71496426481beb6f2a5d4a47fc73cf
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-16 21:33:40 +0100

    Update susfs.c

--- fs/susfs.c
+++ fs/susfs.c
@@ -89,3 +89,54 @@ int __init susfs_init(void)
 late_initcall(susfs_init);
 
 #endif /* CONFIG_KSU_SUSFS && CONFIG_KSU_SUSFS_SPOOF_UNAME */
+
+/* ===================== */
+/* STUBS FOR KERNELSU    */
+/* ===================== */
+
+#ifndef CONFIG_KSU_SUSFS_SUS_PATH
+void susfs_set_i_state_on_external_dir(void __user **u) {}
+void susfs_add_sus_path(void __user **u) {}
+void susfs_add_sus_path_loop(void __user **u) {}
+void susfs_run_sus_path_loop(void) {}
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_SUS_KSTAT
+void susfs_add_sus_kstat(void __user **u) {}
+void susfs_update_sus_kstat(void __user **u) {}
+void susfs_sus_ino_for_generic_fillattr(unsigned long i, struct kstat *s) {}
+void susfs_sus_ino_for_show_map_vma(unsigned long i, dev_t *d, unsigned long *o) {}
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_SUS_MOUNT
+void susfs_set_hide_sus_mnts_for_all_procs(void __user **u) {}
+void susfs_reorder_mnt_id(struct mount *mnt) {}
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_TRY_UMOUNT
+void susfs_add_try_umount(void __user **u) {}
+void susfs_try_umount(uid_t uid) {}
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_ENABLE_LOG
+void susfs_enable_log(void __user **u) {}
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+void susfs_set_cmdline_or_bootconfig(void __user **u) {}
+int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m) { return 0; }
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_OPEN_REDIRECT
+void susfs_add_open_redirect(void __user **u) {}
+struct filename* susfs_get_redirected_path(unsigned long ino) { return NULL; }
+#endif
+
+#ifndef CONFIG_KSU_SUSFS_SUS_MAP
+void susfs_add_sus_map(void __user **u) {}
+#endif
+
+void susfs_set_avc_log_spoofing(void __user **u) {}
+void susfs_get_enabled_features(void __user **u) {}
+void susfs_show_variant(void __user **u) {}
+void susfs_show_version(void __user **u) {}
