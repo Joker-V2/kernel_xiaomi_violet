40c5f4e4cd4d2fb176f157ba2b12865048ca2a53
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-20 17:10:15 +0100

    Update sucompat.h

--- drivers/kernelsu/sucompat.h
+++ drivers/kernelsu/sucompat.h
@@ -2,32 +2,32 @@
 #define __KSU_H_SUCOMPAT
 
 #include <linux/types.h>
+#include <linux/fs.h>  // struct inode
 
-extern bool ksu_su_compat_enabled;
+extern bool ksu_su_compat_enabled; // For SUSFS mode
 
 void ksu_sucompat_init(void);
 void ksu_sucompat_exit(void);
 
-// Handler functions exported for hook_manager
-int ksu_handle_faccessat(
-	int *dfd,
-	const char __user **filename_user,
-	int *mode,
-	int *__unused_flags
-);
+/* Handler functions for manual syscall hooks */
+int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+                         int *mode, int *__unused_flags);
+int ksu_handle_stat(int *dfd, const char __user **filename_user,
+                    int *flags);
 
-int ksu_handle_stat(
-	int *dfd,
-	const char __user **filename_user,
-	int *flags
-);
+/* execve handlers: Manual hooks */
+int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
+                               void *__never_use_argv,
+                               void *__never_use_envp,
+                               int *__never_use_flags);
 
-int ksu_handle_execve_sucompat(
-	int *fd,
-	const char __user **filename_user,
-	void *__never_use_argv,
-	void *__never_use_envp,
-	int *__never_use_flags
-);
+/* execveat handler (for newer kernels or special manual hook) */
+int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+                                 void *__never_use_argv,
+                                 void *__never_use_envp,
+                                 int *__never_use_flags);
 
-#endif /* __KSU_H_SUCOMPAT */
+/* Handler for devpts inode (pts_unix98_lookup) */
+int ksu_handle_devpts(struct inode *inode);
+
+#endif // __KSU_H_SUCOMPAT
