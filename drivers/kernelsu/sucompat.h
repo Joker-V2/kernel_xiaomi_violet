1a5c2c941a0e066f26eb06eb3de6e09e5ad751fe
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-20 22:20:25 +0100

    Update sucompat.h

--- drivers/kernelsu/sucompat.h
+++ drivers/kernelsu/sucompat.h
@@ -1,6 +1,8 @@
 #ifndef __KSU_H_SUCOMPAT
 #define __KSU_H_SUCOMPAT
+
 #include <linux/types.h>
+#include <linux/fs.h>
 
 extern bool ksu_su_compat_enabled;
 
@@ -15,4 +17,13 @@ int ksu_handle_execve_sucompat(const char __user **filename_user,
                                void *__never_use_argv, void *__never_use_envp,
                                int *__never_use_flags);
 
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+extern bool ksu_devpts_hook;
+extern bool susfs_is_sus_su_hooks_enabled;
+extern int susfs_sus_su_working_mode;
+
+void ksu_susfs_enable_sus_su(void);
+void ksu_susfs_disable_sus_su(void);
 #endif
+
+#endif // __KSU_H_SUCOMPAT
