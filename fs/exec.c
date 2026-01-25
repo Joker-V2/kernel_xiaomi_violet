8fa1ee694c83f3691327ac407d61be608e3f3e5a
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-26 00:58:07 +0100

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1900,7 +1900,7 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
 #ifdef CONFIG_KSU
-	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+    ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
 #endif
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
