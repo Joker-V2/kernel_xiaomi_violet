50233da1f6a144c985efd99f30c1838aecc76fb2
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-01-25 22:21:32 +0100

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -301,7 +301,7 @@ static int __bprm_mm_init(struct linux_binprm *bprm)
 	struct vm_area_struct *vma = NULL;
 	struct mm_struct *mm = bprm->mm;
 
-	bprm->vma = vma = kmem_cache_zalloc(vm_area_cachep, GFP_KERNEL);
+	bprm->vma = vma = vm_area_alloc();
 	if (!vma)
 		return -ENOMEM;
 
