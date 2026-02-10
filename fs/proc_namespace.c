ab07cab23b5d720afd3d5aa3eef8a4b9430ecde9
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-02-11 00:46:57 +0100

    Update proc_namespace.c

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,8 +22,8 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_non_su_procs;
 extern bool susfs_is_current_ksu_domain(void);
+bool susfs_hide_sus_mnts_for_all_procs = true; // hide sus mounts for all processes by default
 #endif
 
 static unsigned mounts_poll(struct file *file, poll_table *wait)
@@ -111,12 +111,8 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
-			!susfs_is_current_ksu_domain())
-	{
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
 		return 0;
-	}
 #endif
 
 	if (sb->s_op->show_devname) {
@@ -156,12 +152,8 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
-			!susfs_is_current_ksu_domain())
-	{
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
 		return 0;
-	}
 #endif
 
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
@@ -226,15 +218,11 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	struct mount *r = real_mount(mnt);
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	struct super_block *sb = mnt_path.dentry->d_sb;
-	int err; 
+	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
-			!susfs_is_current_ksu_domain())
-	{
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
 		return 0;
-	}
 #endif
 
 	/* device */
