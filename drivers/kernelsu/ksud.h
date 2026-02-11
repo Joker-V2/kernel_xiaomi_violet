1eec0d2609b75798bfa6d403db85879621390354
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-02-11 17:50:09 +0100

    Update ksud.h

--- drivers/kernelsu/ksud.h
+++ drivers/kernelsu/ksud.h
@@ -36,6 +36,4 @@ int ksu_handle_execveat_ksud(int *fd, struct filename **filename_ptr,
 			     struct user_arg_ptr *argv,
 			     struct user_arg_ptr *envp, int *flags);
 
-int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr, size_t *count_ptr);
-
 #endif
