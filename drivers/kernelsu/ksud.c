409cfec2f90c4650003da836478284eae55cb6d1
mahtiranaskandar-ux <mahtiranaskandar@gmail.com>
2026-02-11 17:49:10 +0100

    Update ksud.c

--- drivers/kernelsu/ksud.c
+++ drivers/kernelsu/ksud.c
@@ -516,7 +516,7 @@ bool is_init_rc(struct file *fp)
 	return true;
 }
 
-int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr, size_t *count_ptr)
+void ksu_handle_sys_read(unsigned int fd)
 {
 	struct file *file = fget(fd);
 #if defined(CONFIG_KSU_SYSCALL_HOOK) || defined(CONFIG_KSU_MANUAL_HOOK) ||     \
